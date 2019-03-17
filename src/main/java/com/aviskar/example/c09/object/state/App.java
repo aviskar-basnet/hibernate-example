d9c66b686321694ae3723bed0b99bd1207da8a18
Aviskar Basnet <aviskarbasnet@yahoo.com>
2019-03-17 23:17:55 +0545

    Latest commit.

--- src/main/java/com/aviskar/example/c09/object/state/App.java
+++ src/main/java/com/aviskar/example/c09/object/state/App.java
@@ -11,20 +11,45 @@ public class App {
 		configuration.addAnnotatedClass(Boy.class);
 		SessionFactory sessionFactory = configuration.buildSessionFactory();
 
+		// sabin is new object
+		// bijay is new object
+		// sabin & bijay becomes managed object after persist method call
+		Session session = sessionFactory.openSession();
 		Boy sabin = new Boy("Sabin");
 		Boy bijay = new Boy("Bijay");
-
-		Session session = sessionFactory.openSession();
 		session.beginTransaction();
 		session.persist(sabin);
 		session.persist(bijay);
 		session.getTransaction().commit();
 		session.close();
 
+		// boy1 is managed object
+		// Hibernate updates the changes to database for managed object
 		session = sessionFactory.openSession();
 		session.beginTransaction();
-		Boy boy = session.find(Boy.class, 1L);
-		session.remove(boy);
+		Boy boy1 = session.find(Boy.class, 1L);
+		boy1.setAddress("Kathmandu");
+		session.getTransaction().commit();
+		session.close();
+
+		// boy2 is managed object
+		// boy2 becomes detached after detach method call
+		// Hibernate doesn't update the changes to database for detached object
+		session = sessionFactory.openSession();
+		session.beginTransaction();
+		Boy boy2 = session.find(Boy.class, 1L);
+		session.detach(boy2);
+		boy1.setAddress("Kathmandu");
+		session.getTransaction().commit();
+		session.close();
+
+		// boy3 is managed object
+		// boy3 becomes removed after remove method call
+		// Hibernate removes removed object from database
+		session = sessionFactory.openSession();
+		session.beginTransaction();
+		Boy boy3 = session.find(Boy.class, 1L);
+		session.remove(boy3);
 		session.getTransaction().commit();
 		session.close();
 
